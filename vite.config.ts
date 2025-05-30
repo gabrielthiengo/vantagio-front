f12b27af31a3bed2515a45cb447524b950a3d39c
gabrielthiengo <gabrielmerces001@hotmail.com>
2025-05-30 14:07:31 -0300

    teste

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,9 @@
 import * as path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'static.json',
-          dest: '',
-        },
-      ],
-    }),
-  ],
+  plugins: [react()],
   server: {
     headers: {
       'Cross-Origin-Embedder-Policy': 'require-corp',
