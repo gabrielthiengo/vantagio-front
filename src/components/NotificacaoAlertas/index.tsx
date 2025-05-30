4f1429a4c5159dbe412f947df7d5b470a096ad5f
gabrielthiengo <gabrielmerces001@hotmail.com>
2025-05-30 16:06:11 -0300

    Melhorias

--- src/components/NotificacaoAlertas/index.tsx
+++ src/components/NotificacaoAlertas/index.tsx
@@ -36,41 +36,43 @@ export default function NotificacaoAlertas() {
   }, []);
 
   return (
-    <div>
-      {!isLoading && notificacoes.length > 0 && (
-        <div className={`w-full grid ${notificacoes.length === 1 ? 'grid-cols-1' : 'grid-cols-2'} gap-3`}>
-          {notificacoes.map((notificacao, index) => {
-            return (
-              <Card
-                key={notificacao.titulo}
-                className={`w-full rounded-md shadow-none border-gray-300 ${
-                  index === notificacoes.length - 1 && notificacoes.length === 3 && 'lg:col-span-2'
-                }`}
-              >
-                <CardContent className="p-4">
-                  <div className="flex items-start gap-4">
-                    {notificacao.icon === 'alert' && <AlertCircle className="text-red-500 w-6 h-6 mt-1" />}
-                    {notificacao.icon === 'trending-up' && <TrendingUp className="text-red-500 w-6 h-6 mt-1" />}
-                    <div>
-                      <h3 className="text-lg font-semibold">{notificacao.titulo}</h3>
-                      <p className="text-xs text-muted-foreground mb-2">{notificacao.descricao}</p>
-                      <Button
-                        variant="default"
-                        className="text-sm"
-                        onClick={() => {
-                          navigate(notificacao.path);
-                        }}
-                      >
-                        {notificacao.tituloBtn}
-                      </Button>
+    notificacoes.length > 0 && (
+      <div>
+        {!isLoading && notificacoes.length > 0 && (
+          <div className={`w-full grid ${notificacoes.length === 1 ? 'grid-cols-1' : 'grid-cols-2'} gap-3`}>
+            {notificacoes.map((notificacao, index) => {
+              return (
+                <Card
+                  key={notificacao.titulo}
+                  className={`w-full rounded-md shadow-none border-gray-300 ${
+                    index === notificacoes.length - 1 && notificacoes.length === 3 && 'lg:col-span-2'
+                  }`}
+                >
+                  <CardContent className="p-4">
+                    <div className="flex items-start gap-4">
+                      {notificacao.icon === 'alert' && <AlertCircle className="text-red-500 w-6 h-6 mt-1" />}
+                      {notificacao.icon === 'trending-up' && <TrendingUp className="text-red-500 w-6 h-6 mt-1" />}
+                      <div>
+                        <h3 className="text-lg font-semibold">{notificacao.titulo}</h3>
+                        <p className="text-xs text-muted-foreground mb-2">{notificacao.descricao}</p>
+                        <Button
+                          variant="default"
+                          className="text-sm"
+                          onClick={() => {
+                            navigate(notificacao.path);
+                          }}
+                        >
+                          {notificacao.tituloBtn}
+                        </Button>
+                      </div>
                     </div>
-                  </div>
-                </CardContent>
-              </Card>
-            );
-          })}
-        </div>
-      )}
-    </div>
+                  </CardContent>
+                </Card>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    )
   );
 }
