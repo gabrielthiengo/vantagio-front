58eb8c17c21ece90263ef72ce723fd065f4d66c2
gabrielthiengo <gabrielmerces001@hotmail.com>
2025-05-30 15:22:24 -0300

    melhorias

--- src/components/Recomendacoes/RecomendacoesTable.tsx
+++ src/components/Recomendacoes/RecomendacoesTable.tsx
@@ -145,7 +145,7 @@ export function RecomendacoesTable({ data, totalPages, currentPage, paginate }: 
             ))}
           </TableHeader>
           <TableBody>
-            {table.getRowModel().rows?.length &&
+            {table.getRowModel().rows?.length > 0 &&
               table.getRowModel().rows.map((row) => (
                 <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
                   {row.getVisibleCells().map((cell) => (
