6bcb7e9307613ae6a0e3586c111754fa28258383
gabrielthiengo <gabrielmerces001@hotmail.com>
2025-06-04 17:19:57 -0300

    Melhorias

--- src/components/LoadingComponent/index.tsx
+++ src/components/LoadingComponent/index.tsx
@@ -2,7 +2,7 @@ import { LoaderCircle } from 'lucide-react';
 
 export default function LoadingComponent() {
   return (
-    <div className="w-full flex flex-col items-center gap-2 border border-gray-300 rounded-md py-5 shadow text-primary">
+    <div className="w-full flex flex-col items-center gap-2 border border-gray-300 bg-white rounded-md py-5 shadow text-primary">
       <LoaderCircle className="animate-spin" size={28} />
 
       <span className="text-sm">Carregando...</span>
--- src/pages/ClienteDetalhe/index.tsx
+++ src/pages/ClienteDetalhe/index.tsx
@@ -34,80 +34,74 @@ export default function ClienteDetalhe() {
 
   return (
     <div>
+      <PageHeader title={`Cliente - ${cliente?.pessoa?.nome}`} icon={<UserRoundSearch size={18} />} />
+
       {!isFetching && (
-        <div>
-          <PageHeader title={`Cliente - ${cliente?.pessoa?.nome}`} icon={<UserRoundSearch size={18} />} />
+        <Card className="rounded shadow-none border border-gray-300 p-4 grid grid-cols-2 gap-3">
+          <Card className="rounded shadow-none border border-gray-300 p-4">
+            <span className="text-gray-600 text-sm">Dados do cliente</span>
 
-          <Card className="rounded shadow-none border border-gray-300 p-4 grid grid-cols-2 gap-3">
-            <Card className="rounded shadow-none border border-gray-300 p-4">
-              <span className="text-gray-600 text-sm">Dados do cliente</span>
+            <Separator className="bg-gray-300 my-2" />
 
-              <Separator className="bg-gray-300 my-2" />
+            <div className="grid grid-cols-[1fr_2fr] gap-2 text-sm mt-4">
+              <span>CPF:</span>
+              <span className="text-right">
+                {cliente?.pessoa?.cpf ? formatarCPF(cliente?.pessoa?.cpf) : <NaoCadastrado />}
+              </span>
 
-              <div className="grid grid-cols-[1fr_2fr] gap-2 text-sm mt-4">
-                <span>CPF:</span>
-                <span className="text-right">
-                  {cliente?.pessoa?.cpf ? formatarCPF(cliente?.pessoa?.cpf) : <NaoCadastrado />}
-                </span>
+              <span>RG:</span>
+              <span className="text-right">{cliente?.pessoa?.rg ? cliente?.pessoa?.rg : <NaoCadastrado />}</span>
 
-                <span>RG:</span>
-                <span className="text-right">{cliente?.pessoa?.rg ? cliente?.pessoa?.rg : <NaoCadastrado />}</span>
+              <span>Aniversário:</span>
+              <span className="text-right">
+                {cliente?.pessoa?.dataNascimento ? (
+                  formatarData(String(cliente?.pessoa?.dataNascimento), true)
+                ) : (
+                  <NaoCadastrado />
+                )}
+              </span>
 
-                <span>Aniversário:</span>
-                <span className="text-right">
-                  {cliente?.pessoa?.dataNascimento ? (
-                    formatarData(String(cliente?.pessoa?.dataNascimento), true)
-                  ) : (
-                    <NaoCadastrado />
-                  )}
-                </span>
+              <span>Gênero:</span>
+              <span className="text-right">
+                {cliente?.pessoa?.genero ? cliente?.pessoa?.genero : <NaoCadastrado />}
+              </span>
 
-                <span>Gênero:</span>
-                <span className="text-right">
-                  {cliente?.pessoa?.genero ? cliente?.pessoa?.genero : <NaoCadastrado />}
-                </span>
+              <span>Email:</span>
+              <span className="text-right">{cliente?.email ? cliente?.email : <NaoCadastrado />}</span>
 
-                <span>Email:</span>
-                <span className="text-right">{cliente?.email ? cliente?.email : <NaoCadastrado />}</span>
+              <span>Telefone:</span>
+              <span className="text-right">
+                {cliente?.telefone ? formatarTelefone(cliente?.telefone) : <NaoCadastrado />}
+              </span>
 
-                <span>Telefone:</span>
-                <span className="text-right">
-                  {cliente?.telefone ? formatarTelefone(cliente?.telefone) : <NaoCadastrado />}
-                </span>
-
-                <span>Cadastro:</span>
-                <span className="text-right">
-                  {cliente?.dataCadastroExterno ? (
-                    formatarData(String(cliente?.dataCadastroExterno))
-                  ) : (
-                    <NaoCadastrado />
-                  )}
-                </span>
-              </div>
-            </Card>
-            <Card className="rounded shadow-none border border-gray-300 p-4">
-              <span className="text-gray-600 text-sm">Pedidos do cliente</span>
-
-              <Separator className="bg-gray-300 my-2" />
-
-              {id && <PedidosCliente clienteId={Number(id)} />}
-            </Card>
-            <Card className="rounded shadow-none border border-gray-300 p-4">
-              <span className="text-gray-600 text-sm">Automações</span>
-
-              <Separator className="bg-gray-300 my-2" />
-
-              {id && <AutomacoesCliente clienteId={Number(id)} />}
-            </Card>
-            <Card className="rounded shadow-none border border-gray-300 p-4">
-              <span className="text-gray-600 text-sm">Contatos com cliente</span>
-
-              <Separator className="bg-gray-300 my-2" />
-
-              {id && cliente && <ContatosCliente cliente={cliente} />}
-            </Card>
+              <span>Cadastro:</span>
+              <span className="text-right">
+                {cliente?.dataCadastroExterno ? formatarData(String(cliente?.dataCadastroExterno)) : <NaoCadastrado />}
+              </span>
+            </div>
           </Card>
-        </div>
+          <Card className="rounded shadow-none border border-gray-300 p-4">
+            <span className="text-gray-600 text-sm">Pedidos do cliente</span>
+
+            <Separator className="bg-gray-300 my-2" />
+
+            {id && <PedidosCliente clienteId={Number(id)} />}
+          </Card>
+          <Card className="rounded shadow-none border border-gray-300 p-4">
+            <span className="text-gray-600 text-sm">Automações</span>
+
+            <Separator className="bg-gray-300 my-2" />
+
+            {id && <AutomacoesCliente clienteId={Number(id)} />}
+          </Card>
+          <Card className="rounded shadow-none border border-gray-300 p-4">
+            <span className="text-gray-600 text-sm">Contatos com cliente</span>
+
+            <Separator className="bg-gray-300 my-2" />
+
+            {id && cliente && <ContatosCliente cliente={cliente} />}
+          </Card>
+        </Card>
       )}
 
       {isFetching && <LoadingComponent />}
