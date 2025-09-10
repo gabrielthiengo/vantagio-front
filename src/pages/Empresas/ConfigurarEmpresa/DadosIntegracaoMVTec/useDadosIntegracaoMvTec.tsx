ffac1079df9238502ec589fbc2fe8c36d3972eda
gabrielthiengo <gabrielmerces001@hotmail.com>
2025-09-10 13:30:54 -0300

    Implementações de configurações para o mvtec

--- src/pages/Empresas/ConfigurarEmpresa/DadosIntegracaoMVTec/useDadosIntegracaoMvTec.tsx
+++ src/pages/Empresas/ConfigurarEmpresa/DadosIntegracaoMVTec/useDadosIntegracaoMvTec.tsx
@@ -1,6 +1,6 @@
 import { IConfiguracaoDatabaseMV } from '../../../../interfaces/IConfiguracaoDatabaseMV';
 import { IQueryDatabaseMV } from '../../../../interfaces/IQueryDatabaseMV';
-import { IScheduleJobMV } from '../../../../interfaces/IScheduleJobMv';
+import { IScheduleJobMV } from '../../../../interfaces/IScheduleJobMV';
 import { apiRequest } from '../../../../services/apiRequest';
 import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
